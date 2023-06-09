fe327187f56c717fd3dd4e63e499c7d0c516abc6
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-06-09 13:33:38 -0300

    Alert Mobile

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let btn = document.querySelector('.voltar');
 let widthScreen = window.screen.width;
-alert("Está página ainda não foi adaptada para dispositivos móveis");
 
-if (widthScreen < 1220){
-    alert("Está página ainda não foi adaptada para dispositivos móveis");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    if(widthScreen < 1200){
+        alert("Está página ainda não foi adaptada para dispositivos móveis!")
+    }
+  });
+
 
 btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
-    behavior: "smooth",
+    behavior: "smooth"
 });
